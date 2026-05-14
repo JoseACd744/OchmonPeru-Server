5622dba83363104bea43c9999d6823ccb6517c96
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2026-05-14 15:20:01 -0500

    feat: actualizar versión del prompt a 1 en la clase OpenAIService

--- src/services/openaiService.js
+++ src/services/openaiService.js
@@ -184,7 +184,7 @@ class OpenAIService {
       const requestParams = {
         prompt: { 
           id: promptId,
-          version: "23" // o especificar versión como "3"
+          version: "1" // o especificar versión como "3"
         },
         input: input,
         text: {
